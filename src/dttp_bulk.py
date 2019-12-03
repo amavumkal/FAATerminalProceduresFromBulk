c6f5ac33c1b3a332a65fd3b60631b0ed30fbd244
Arun Mavumkal <av8r88@gmail.com>
2019-12-03 05:43:42 -0500

    bug fix - creates meta directory if directory doesn't already exist

--- src/dttp_bulk.py
+++ src/dttp_bulk.py
@@ -8,8 +8,9 @@ import os
 import time
 from png import *
 
+
 class DttpBulk:
-    def __init__(self, download_directory):  
+    def __init__(self, download_directory):
         self.__charts = None
         self.__DOWNLOAD_DIRECTORY = download_directory
         if not os.path.exists(self.__DOWNLOAD_DIRECTORY):
@@ -18,7 +19,7 @@ class DttpBulk:
         #     if os.path.isdir(self.__DOWNLOAD_DIRECTORY):
         #         os.system('rm -rf ' + self.__DOWNLOAD_DIRECTORY + '/*')
 
-    def set_charts(self,charts_in):
+    def set_charts(self, charts_in):
         self.__charts = charts_in
 
     def __clear_dload_dir(self):
@@ -28,8 +29,8 @@ class DttpBulk:
             elif os.path.isfile(sub_dir):
                 os.remove(sub_dir)
 
-    #pre: method takes no arguments.
-    #post: downloads charts from faa digital proand above, the specified commit will be merged to the current active branch. Most of the time, you will want to merge a branch with ducts site and saves to
+    # pre: method takes no arguments.
+    # post: downloads charts from faa digital proand above, the specified commit will be merged to the current active branch. Most of the time, you will want to merge a branch with ducts site and saves to
     def download(self):
         META_FILE_DIR = './meta'
         print('Downloading metafile')
@@ -54,7 +55,7 @@ class DttpBulk:
             return self.__charts
         else:
             raise Exception('Charts list is empty')
-   
+
     def organize_charts(self):
         if not self.__charts:
             raise Exception('Charts list is empty')
@@ -67,7 +68,7 @@ class DttpBulk:
                 continue
             else:
                 chart_dict[PDF_NAME] = chart
-        
+
         for file_name in os.listdir('./'):
             if file_name in chart_dict:
                 chart = chart_dict[file_name]
@@ -92,6 +93,8 @@ class DttpBulk:
         URL = 'http://aeronav.faa.gov/d-tpp/%s/xml_data/d-tpp_Metafile.xml' % (DttpBulk.get_four_digit_cycle())
         print(URL)
         request = requests.get(URL)
+        if not os.path.isdir(dest_directory):
+            os.mkdir(dest_directory)
         open(dest_directory + '/d-TPP_Metafile.xml', 'wb').write(request.content)
 
     @staticmethod
@@ -139,8 +142,8 @@ class DttpBulk:
             cycle_day = "0" + cycle_day
         return (cycle_year + cycle_month + cycle_day)
 
-    @staticmethod 
-    def get_four_digit_cycle(): 
+    @staticmethod
+    def get_four_digit_cycle():
         PRESENT = datetime.datetime.now()  # present time
         TIME_DELTA = datetime.timedelta(days=28)  # used to increase date by 28 days. FAA chart cycle
         CYCLE_BASE = datetime.datetime(2016, 8, 18)  # reference point for faa cycles
@@ -153,6 +156,7 @@ class DttpBulk:
         if (len(cycle_month) == 1):  # if single digit month adds 0 to resulting string
             cycle_month = "0" + cycle_month
         return (cycle_year + cycle_month)
+
 
 if __name__ == "__main__":
     convert = Convert_To_PNG()
